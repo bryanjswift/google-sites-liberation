5ce81f3b293e21ecc9b58720d8d6fed1bbfc4f9b
Bryan Swift <bryan.j.swift@gmail.com>
2010-02-15 18:36:50 -0500

    Catch all try / catch block in process
    If an exception is thrown while trying to clean things up then just
    return the original. The most likely culprit of an exception is invalid
    markup. Before returning an empty node sequence for 'empty' <a> tags
    make sure the text is empty as well.

--- src/main/scala/com/knovel/export/XhtmlCleanup.scala
+++ src/main/scala/com/knovel/export/XhtmlCleanup.scala
@@ -6,13 +6,16 @@ import scala.xml.{Elem,Node,XML}
 import scala.xml.transform.{RewriteRule,RuleTransformer}
 
 object XhtmlCleanup {
-	def process(entry:BasePageEntry[_]) = {
-		val content = EntryUtils.getXhtmlContent(entry)
-		val block = XML.loadString(content)
-		val result = cleanEmptyLinks(cleanFontTags(block))
-		EntryUtils.setContent(entry, result.toString)
-		entry
-	}
+	def process(entry:BasePageEntry[_]) =
+		try {
+			val content = EntryUtils.getXhtmlContent(entry)
+			val block = XML.loadString(content)
+			val result = cleanEmptyLinks(cleanFontTags(block))
+			EntryUtils.setContent(entry, result.toString)
+			entry
+		} catch {
+			case e => entry
+		}
 	private[export] object cleanFontTags extends RuleTransformer(new RewriteRule {
 		override def transform(n:Node) =
 			n match {
@@ -24,7 +27,7 @@ object XhtmlCleanup {
 		override def transform(n:Node) =
 			n match {
 				case a @ Elem(_,"a",_,_,_*) => {
-					if (a.length == 1) Seq[Node]()
+					if (a.length == 1 && a.text == "") Seq[Node]()
 					else a
 				}
 				case other => other
