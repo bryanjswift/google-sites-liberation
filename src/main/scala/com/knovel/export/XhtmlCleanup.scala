567ae225c5bb7841bc9112d8322f8071b535afb1
Bryan Swift <bryan.j.swift@gmail.com>
2010-02-15 15:49:32 -0500

    Add XhtmlCleanup object
    XhtmlCleanup.process cleans up the content of a BasePageEntry and
    returns XML with empty links and all font tags removed. Add tests for
    the two cleanup methods.

---
+++ src/main/scala/com/knovel/export/XhtmlCleanup.scala
@@ -0,0 +1,33 @@
+package com.knovel.export
+
+import com.google.gdata.data.sites.BasePageEntry
+import com.google.sites.liberation.util.EntryUtils
+import scala.xml.{Elem,Node,NodeSeq,XML}
+import scala.xml.transform.{RewriteRule,RuleTransformer}
+
+object XhtmlCleanup {
+	def process(entry:BasePageEntry[_]) = {
+		val content = EntryUtils.getXhtmlContent(entry)
+		val block = XML.loadString(content)
+		val result = cleanFontTags(block)
+		EntryUtils.setContent(entry, result.toString)
+		entry
+	}
+	private[export] object cleanFontTags extends RuleTransformer(new RewriteRule {
+		override def transform(n:Node) =
+			n match {
+				case Elem(prefix,"font",attributes,scope,children) => children
+				case other => other
+			}
+	})
+	private[export] object cleanEmptyLinks extends RuleTransformer(new RewriteRule {
+		override def transform(n:Node) =
+			n match {
+				case a @ Elem(_,"a",_,_,_*) => {
+					if (a.length == 1) Seq[Node]()
+					else a
+				}
+				case other => other
+			}
+	})
+}
---
+++ src/test/scala/com/knovel/export/XhtmlCleanupTests.scala
@@ -0,0 +1,31 @@
+package com.knovel.export
+
+import junit.framework.TestCase
+import org.junit.Assert._
+
+class XhtmlCleanupTest extends TestCase {
+	override def setUp { }
+	override def tearDown { }
+	def testFontBlocksGone = {
+		val div =
+			<div>
+				<font><a href="test.html">This is some text</a></font>
+				<p><font>More text</font></p>
+			</div>
+		val result = XhtmlCleanup.cleanFontTags(div)
+		assertEquals(0,(result \\ "font").length)
+		assertEquals("This is some text",(result \ "a").text)
+		assertEquals("More text",(result \ "p").text)
+	}
+	def testEmptyLinksGone = {
+		val div =
+			<div>
+				<font><a href="test.html"></a>This is some text</font>
+				<p><a href="test.html" />More text</p>
+			</div>
+		val result = XhtmlCleanup.cleanEmptyLinks(div)
+		assertEquals(0,(result \\ "a").length)
+		assertEquals("This is some text",(result \ "font").text)
+		assertEquals("More text",(result \ "p").text)
+	}
+}
